1e892299dc5021836c1e0197ad0517931360c619
Nicholas J <nicholas.a.johns5@gmail.com>
2015-04-17 23:12:47 -0400

    Initial commit

---
+++ src/Rql/Def/Query/QueryType.php
@@ -0,0 +1,13 @@
+<?php
+namespace Rql\Def\Query;
+
+class QueryType
+{
+  const START = 1;
+  const R_CONTINUE = 2;
+  /**
+   * (see [Response]).
+   */
+  const STOP = 3;
+  const NOREPLY_WAIT = 4;
+}
---
+++ src/Rql/Def/VersionDummy/Protocol.php
@@ -0,0 +1,8 @@
+<?php
+namespace Rql\Def\VersionDummy;
+
+class Protocol
+{
+  const PROTOBUF = 656407617;
+  const JSON = 2120839367;
+}
